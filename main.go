5b9825dd6fc88e7af06c74129d09e52230836aac
Renato Filho <rfiilho@gmail.com>
2023-10-04 23:59:18 -0300

    ORM + Handler

--- main.go
+++ main.go
@@ -3,16 +3,12 @@ package main
 import (
 	"fmt"
 
-	"github.com/rfiilho/api-go-rest/models"
+	"github.com/rfiilho/api-go-rest/database"
 	"github.com/rfiilho/api-go-rest/routes"
 )
 
 func main() {
-	models.Personalidades = []models.Personalidade{
-		{Id: 1, Nome: "Nome 1", Historia: "Historia 1"},
-		{Id: 2, Nome: "Nome 2", Historia: "Historia 2"},
-	}
-
+	database.Connect()
 	fmt.Println("Iniciando o servidor Rest com Go")
 	routes.HandleResquest()
 }
