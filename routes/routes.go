5d29ace84397ad2e3a196e9983e9df0b1de67466
Renato Filho <rfiilho@gmail.com>
2023-10-30 21:56:34 -0300

    CORS

--- routes/routes.go
+++ routes/routes.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/rfiilho/api-go-rest/controllers"
 	"github.com/rfiilho/api-go-rest/middleware"
@@ -18,5 +19,5 @@ func HandleResquest() {
 	r.HandleFunc("/api/personalidades", controllers.CriaPersonalidade).Methods("Post")
 	r.HandleFunc("/api/personalidades/{id}", controllers.DeletaPersonalidade).Methods("Delete")
 	r.HandleFunc("/api/personalidades/{id}", controllers.EditaPersonalidade).Methods("Put")
-	log.Fatal(http.ListenAndServe(":8000", r))
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(handlers.AllowedOrigins([]string{"*"}))(r)))
 }
